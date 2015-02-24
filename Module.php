8c177a7de89826e9dedcd04b7c2504f751c500f1
BnitoBzh <fievet.benoit@gmail.com>
2015-02-24 14:55:21 +0100

    Add Javascript library

--- Module.php
+++ Module.php
@@ -12,6 +12,8 @@
 
 namespace ZendIntercom;
 
+use Zend\Mvc\MvcEvent;
+
 /**
  * Class Module
  *
@@ -19,6 +21,22 @@ namespace ZendIntercom;
  */
 class Module
 {
+    protected $serviceManager;
+    protected $config;
+    
+    /**
+     * @param MvcEvent $event
+     */
+    public function onBootstrap(MvcEvent $event)
+    {
+        $this->serviceManager = $event->getApplication()->getServiceManager();
+        $this->config = $this->serviceManager->get('Config');
+    
+        if ($this->config['zend-intercom']['enable-javascript-integration']) {
+            $this->setupJavascriptLogging($event);
+        }
+    }
+    
     /**
      * @return array
      */
@@ -35,5 +53,23 @@ class Module
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+    
+    /**
+     * Adds the necessary javascript, tries to prepend
+     *
+     * @param MvcEvent $event
+     */
+    protected function setupJavascriptLogging(MvcEvent $event)
+    {
+        $appID = $this->config['zend-intercom']['app_id'];
+        $auth = $this->serviceManager->get('zfcuser_auth_service');
+        if ($auth->hasIdentity()) {
+            $user = $auth->getIdentity();
+            $viewHelper = $event->getApplication()->getServiceManager()->get('viewhelpermanager')->get('headscript');
+            $viewHelper->appendScript(sprintf('window.intercomSettings = {user_id: "%s",email: "%s",app_id: "%s"};', $user->getId(), $user->getEmail(), $appID));
+            $viewHelper->appendScript(sprintf("(function(){var w=window;var ic=w.Intercom;if(typeof ic===\"function\"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/%s';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()", $appID));
+        }
+        
     }
 }
